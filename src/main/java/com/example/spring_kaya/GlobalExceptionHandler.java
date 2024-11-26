a2d607e0b25fd5ca9c8054784d33069ad2d443d7
NKevintai16 <N.kevintai16@gmail.com>
2024-11-26 14:46:05 -0600

    Created GlobalExceptionHandler.java
    Filled out core code

--- src/main/java/com/example/spring_kaya/GlobalExceptionHandler.java
+++ src/main/java/com/example/spring_kaya/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package com.example.spring_kaya;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    //Handle validation errors
+    @ExceptionHandler
+    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // Extract validation error details
+        ex.getBindingResult().getAllErrors().forEach(objectError -> {
+            String fieldName = ((FieldError) errors).getField();
+            String errorMessage = objectError.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occured: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
