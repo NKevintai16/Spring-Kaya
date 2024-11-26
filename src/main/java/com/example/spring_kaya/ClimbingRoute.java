72b63f1b3c112404d65c29a369a6d2a0a0bc4236
NKevintai16 <N.kevintai16@gmail.com>
2024-11-26 13:43:00 -0600

    Added to pom.xml the dependencies: Jakarta validation 3.0.2 Hibernate validator 8.0.1.Final
    Added validators to:
    routeName
    routeDifficulty
    location
    notes
    
    Entries can not be blank and size are limited, also added error messages.

--- src/main/java/com/example/spring_kaya/ClimbingRoute.java
+++ src/main/java/com/example/spring_kaya/ClimbingRoute.java
@@ -1,7 +1,10 @@
 package com.example.spring_kaya;
 
 import jakarta.persistence.*;
+import jakarta.validation.*;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.antlr.v4.runtime.misc.NotNull;
 
@@ -19,19 +22,24 @@ public class ClimbingRoute {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Route Name cannot be blank")
+    @Size(max = 100, message = "Route name must be less than 100 characters")
     @Column (nullable = false)
     private String routeName;
 
-
+    @NotBlank(message = "Route difficulty cannot be blank")
     @Column(nullable = false)
     private String routeDifficulty;
 
+    @NotBlank(message = "Location cannot be blank")
+    @Size(max = 150, message = "Location must be less than 150 characters")
     @Column(nullable = false)
     private String location;
 
     @Column(nullable = true)
     private LocalDateTime dateCompleted;
 
+    @Size(max = 500, message = "Notes must be less than 500 characters")
     @Column(length = 500)
     private String notes;
 }
