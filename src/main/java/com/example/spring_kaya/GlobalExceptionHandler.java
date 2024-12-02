63ada5d08ca42e0c3d98042f093e5f4f4c610e2b
NKevintai16 <N.kevintai16@gmail.com>
2024-12-02 10:43:24 -0600

    Tested GlobalExceptionHandler.java
    If location, name, or difficulty are blank will return an error.

--- src/main/java/com/example/spring_kaya/GlobalExceptionHandler.java
+++ src/main/java/com/example/spring_kaya/GlobalExceptionHandler.java
@@ -4,30 +4,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     //Handle validation errors
-    @ExceptionHandler
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-
-        // Extract validation error details
-        ex.getBindingResult().getAllErrors().forEach(objectError -> {
-            String fieldName = ((FieldError) errors).getField();
-            String errorMessage = objectError.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
-        return new ResponseEntity<>("An unexpected error occured: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
